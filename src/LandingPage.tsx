6163140d87e10e2a36c223c178c22bfb8da4d231
castroalvesadvogados <rafael@castroalvesadvogados.com>
2026-04-04 11:17:50 -0300

    Update LandingPage.tsx

--- src/LandingPage.tsx
+++ src/LandingPage.tsx
@@ -353,7 +353,6 @@ const Team = () => {
                   src={member.image} 
                   alt={member.name} 
                   className="w-full h-full object-cover grayscale hover:grayscale-0 transition-all duration-700"
-                  referrerPolicy="no-referrer"
                 />
               </div>
             </div>
